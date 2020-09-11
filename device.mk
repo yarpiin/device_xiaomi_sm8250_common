2bb38b6b40e5566a2b69a45422e3f5816f53453a
Jyotiraditya <imjyotiraditya@pm.me>
2020-09-11 18:19:05 +0000

    umi: Build signapk jar
    Test: build
    Signed-off-by: Jyotiraditya <imjyotiraditya@pm.me>
    Change-Id: Iab633fc9d450ec1354308a87f300550df73553bb
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- device.mk
+++ device.mk
@@ -147,6 +147,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/fstab.qcom
 
+# Signapk
+PRODUCT_HOST_PACKAGES += \
+    signapk
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
