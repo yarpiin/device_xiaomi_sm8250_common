468ce91d495af7e920fbd18613689257d9d6031a
ArianK16a <arian.kulmer@gmail.com>
2020-11-29 22:48:29 +0000

    umi: Import XiaomiParts

--- device.mk
+++ device.mk
@@ -38,6 +38,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     Snap
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-parts.xml
+
 # Display
 PRODUCT_PACKAGES += \
     libdisplayconfig \
