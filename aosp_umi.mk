3488fe80e9c345d5e7e144db66450c96cbd18695
dataoutputstream <mb150996@gmail.com>
2020-11-29 22:25:25 +0000

    umi: Inhert telephony and aosp base

--- aosp_umi.mk
+++ aosp_umi.mk
@@ -9,6 +9,10 @@ $(call inherit-product, device/xiaomi/umi/device.mk)
 # Inherit some common Aosp stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_umi
 PRODUCT_DEVICE := umi
