5d3a8cc9939906e995c6f1edcfd9b5debef5498b
dataoutputstream <mb150996@gmail.com>
2020-11-29 21:03:55 +0000

    umi: Include fstab in the ramdisk

--- device.mk
+++ device.mk
@@ -12,6 +12,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-aosp
 
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
+
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_SHIPPING_API_LEVEL := 29
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
