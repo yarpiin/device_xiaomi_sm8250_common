d6cdee5614b5b4fc66335880f346ed36e7aeb91f
ZIDAN44 <zidan44@pixelexperience.org>
2020-12-05 09:49:30 +0000

    umi: Update Device Build FP From Sunfish October Patch

--- aosp_umi.mk
+++ aosp_umi.mk
@@ -20,9 +20,14 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 10
 PRODUCT_MANUFACTURER := Xiaomi
 
+# Fingerprint
+BUILD_FINGERPRINT := google/sunfish/sunfish:11/RP1A.201005.006/6828489:user/release-keys
+BUILD_DESCRIPTION := sunfish-user 11 RP1A.201005.006 6828489 release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="qti/qssi/qssi:10/QKQ1.191117.002/V12.0.8.0.QJACNXM:user/release-keys" \
-    PRODUCT_NAME="umi" \
-    TARGET_DEVICE="umi"
+    PRIVATE_BUILD_DESC="$(BUILD_DESCRIPTION)"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
