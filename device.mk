5a4138ad0454aed3d3dee494e93c2af823283d0d
dataoutputstream <mb150996@gmail.com>
2021-01-07 21:23:47 +0000

    umi: build contexthal

--- device.mk
+++ device.mk
@@ -64,6 +64,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     CameraGo
 
+# Context hub HAL
+PRODUCT_PACKAGES += \
+    android.hardware.contexthub@1.0-impl.generic \
+    android.hardware.contexthub@1.0-service
+
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts
