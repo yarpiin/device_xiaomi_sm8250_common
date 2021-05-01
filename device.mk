84bd13171ab680007a8ded5d60fc74c3ca437f8b
yarpiin <yarpiin@gmail.com>
2021-05-01 14:11:28 +0100

    umi: drop interfaces

--- device.mk
+++ device.mk
@@ -359,7 +359,7 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/xiaomi 
+    hardware/xiaomi
     
 # Telephony
 PRODUCT_PACKAGES += \
