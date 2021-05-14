98cbe7a94ba159b87f2d60077c08e09b3413df36
chandu078 <chandudyavanapelli03@gmail.com>
2021-05-14 22:32:46 +0100

    umi: common: Set default sounds

--- device.mk
+++ device.mk
@@ -363,6 +363,12 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi
+
+# Sounds
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.config.ringtone=Ganymede.ogg \
+    ro.config.notification_sound=Carme.ogg \
+    ro.config.alarm_alert=Argon.ogg
     
 # Telephony
 PRODUCT_PACKAGES += \
