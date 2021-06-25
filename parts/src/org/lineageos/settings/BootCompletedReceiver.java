aafc48f3952dcdde1b6fc2941066ab1520bd817c
yarpiin <yarpiin@gmail.com>
2021-06-25 19:36:26 +0100

    umi: parts: clean up
    Remove Dirac abd DC Dimming

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -25,7 +25,8 @@ import android.provider.Settings;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import org.lineageos.settings.doze.DozeUtils;
-import org.lineageos.settings.dirac.DiracUtils;
+import org.lineageos.settings.thermal.ThermalUtils;
+
 import org.lineageos.settings.fod.FodUtils;
 import vendor.xiaomi.hardware.touchfeature.V1_0.ITouchFeature;
 import org.lineageos.settings.thermal.ThermalUtils;
