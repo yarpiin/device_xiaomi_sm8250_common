d362978230b3f5cab35478b354d906629176ac1b
yarpiin <yarpiin@gmail.com>
2021-07-13 23:22:32 +0100

    Umi: Init Evolution 5.9

--- init/init_umi.cpp
+++ init/init_umi.cpp
@@ -108,9 +108,9 @@ void load_umi() {
     property_override("ro.product.system.model", "Mi 10");
     property_override("ro.build.product", "umi");
     property_override("ro.product.device", "umi");
-    property_override("ro.build.description", "redfin-user 11 RQ3A.210605.005 7349499 release-keys");
+    property_override("ro.build.description", "redfin-user 11 RQ3A.210705.001 7380771 release-keys");
     property_override_multifp("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.bootimage.build.fingerprint",
-        "ro.vendor.build.fingerprint", "google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys");
+        "ro.vendor.build.fingerprint", "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys");
 }
 
 void vendor_load_properties() {
