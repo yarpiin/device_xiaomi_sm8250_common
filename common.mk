8b0c3e01754bda415f78b5882900081a702447a9
bgcngm <bgcngm@gmail.com>
2021-07-24 21:28:28 +0100

    Revert "umi: Implement SunlightEnhancement LiveDisplay HAL"
    This reverts commit 359869cff27bdb9274e804383091f29ece846e31.

--- common.mk
+++ common.mk
@@ -160,8 +160,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm \
-    vendor.lineage.livedisplay@2.0-service.kona
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Low power Whitelist
 PRODUCT_COPY_FILES += \
