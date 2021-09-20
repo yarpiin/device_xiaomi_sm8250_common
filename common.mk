9a43f14d9ad182e5eab4d0180d24f06836e78753
SebaUbuntu <barezzisebastiano@gmail.com>
2021-09-20 19:36:17 +0100

    sm8250-common: move display configs where they belong

--- common.mk
+++ common.mk
@@ -125,8 +125,45 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3-service.clearkey
 
-# Display
-$(call inherit-product, $(LOCAL_PATH)/kona/display/display.mk)
+# Display product definitions
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
+    vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.display.composer-service \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
+    gralloc.kona \
+    lights.kona \
+    hwcomposer.kona \
+    memtrack.kona \
+    libsdmcore \
+    libsdmutils \
+    libqdMetaData \
+    libdisplayconfig.qti \
+    libdisplayconfig.vendor \
+    vendor.qti.hardware.display.mapper@1.0.vendor \
+    vendor.qti.hardware.display.mapper@1.1.vendor \
+    vendor.qti.hardware.display.mapper@2.0.vendor \
+    vendor.qti.hardware.display.mapper@3.0.vendor \
+    vendor.qti.hardware.display.mapper@4.0.vendor \
+    modetest \
+    libtinyxml 
+    
+PRODUCT_PACKAGES += \
+    vendor.display.config@1.0 \
+    vendor.display.config@1.0.vendor \
+    vendor.display.config@1.1.vendor \
+    vendor.display.config@1.2.vendor \
+    vendor.display.config@1.3.vendor \
+    vendor.display.config@1.4.vendor \
+    vendor.display.config@1.5.vendor \
+    vendor.display.config@1.6.vendor \
+    vendor.display.config@1.7.vendor \
+    vendor.display.config@1.8.vendor \
+    vendor.display.config@1.9.vendor \
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0.vendor 
     
 # Device-specific settings
 PRODUCT_PACKAGES += \
