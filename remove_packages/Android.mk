8b7d163d3f5bbb5d4bd93ffdb6db4fb596f77c3c
yarpiin <yarpiin@gmail.com>
2021-12-05 23:24:40 +0000

    sm8250-common: nuke more bloat

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -5,7 +5,9 @@ LOCAL_MODULE := RemovePackages
 LOCAL_SRC_FILES := false
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
-LOCAL_OVERRIDES_PACKAGES := Email Exchange Exchange2 UnifiedEmail PhotoTable Eleven Camera2 TipsPrebuilt SafetyHubPrebuilt RecorderPrebuilt
+LOCAL_OVERRIDES_PACKAGES := Email Exchange Exchange2 UnifiedEmail PhotoTable Eleven Camera2 TipsPrebuilt 
+LOCAL_OVERRIDES_PACKAGES += YouTube YouTubeMusicPrebuilt SafetyHubPrebuilt RecorderPrebuilt
+LOCAL_OVERRIDES_PACKAGES += PrebuiltGmail PlayAutoInstallConfig DocumentsUIGoogle
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := platform
 include $(BUILD_PREBUILT)
