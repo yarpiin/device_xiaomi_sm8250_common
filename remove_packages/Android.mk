c7471a31a510dc128892381fe8a1e869a468ca51
yarpiin <yarpiin@gmail.com>
2021-12-11 15:58:00 +0000

    sm8250-common: Bring back DocumentsUIGoogle

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -7,7 +7,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_OVERRIDES_PACKAGES := Email Exchange Exchange2 UnifiedEmail PhotoTable Eleven TipsPrebuilt 
 LOCAL_OVERRIDES_PACKAGES += YouTube YouTubeMusicPrebuilt SafetyHubPrebuilt RecorderPrebuilt
-LOCAL_OVERRIDES_PACKAGES += PrebuiltGmail PlayAutoInstallConfig DocumentsUIGoogle
+LOCAL_OVERRIDES_PACKAGES += PrebuiltGmail PlayAutoInstallConfig
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := platform
 include $(BUILD_PREBUILT)
