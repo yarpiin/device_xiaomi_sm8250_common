8995cb9313898137b8041a2d5294f4f45d896694
hh2333 <tongsj2333@gmail.com>
2022-01-04 16:41:56 +0000

    sm8250-common: Commonize IFAAManager & enable boot_jar
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: I605593ad59c755b65d8a4f4358b3531d97e792bd

--- common.mk
+++ common.mk
@@ -218,8 +218,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager
 
-#PRODUCT_BOOT_JARS += \
-#    org.ifaa.android.manager
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
 
 # Init
 PRODUCT_COPY_FILES += \
