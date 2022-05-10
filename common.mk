e95247ba777915fdb06a8131f81c74794a360f29
SebaUbuntu <barezzisebastiano@gmail.com>
2022-05-10 08:25:38 +0100

    sm8250-common: Move to common Xiaomi touch HIDL Change-Id: I07fb0b9c1b2a39d8630fe197e723f4a90ce4c9a8

--- common.mk
+++ common.mk
@@ -445,6 +445,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.xiaomi
+
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2
