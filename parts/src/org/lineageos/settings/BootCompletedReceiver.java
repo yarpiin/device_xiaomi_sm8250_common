62e028f1b8a4b7862bf026cc1099d012f5669752
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2022-09-15 20:28:26 +0100

    sm8250-common: parts: Implement Pocket Detection service This is a basic pocket detection service that uses Xiaomi's custom "large area touch sensor" to prevent accidentally triggering lockscreen controls while the device is in pocket. We cannot use traditional proximity + light sensor based pocket detection, as we have ultrasound proximity sensor.
    Inspired by the same usage in stock MIUI:
    https://gist.github.com/ghostrider-reborn/4c7f24151db7639a0244caa87178e521
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,6 +23,7 @@ import android.content.Intent;
 import android.util.Log;
 
 import org.lineageos.settings.doze.DozeUtils;
+import org.lineageos.settings.doze.PocketService;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -33,5 +34,8 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DozeUtils.checkDozeService(context);
+
+        // Pocket
+        PocketService.startService(context);
     }
 }
